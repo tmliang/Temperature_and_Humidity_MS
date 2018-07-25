3c61f84a6b85bd1f6bf9b3a2721a795ae7ec0d45
天蓝蓝铭靓靓 <1062075993@qq.com>
2018-07-25 21:50:16 +0800

    Update chartwidget.cpp

--- qt/QTChartApp/chartwidget.cpp
+++ qt/QTChartApp/chartwidget.cpp
@@ -1,4 +1,4 @@
-//
+/*
 // The MIT License (MIT)
 //
 // Copyright (c) 2018 007小组
@@ -6,7 +6,7 @@
 // Date : 2018.7.25
 //
 // Description : 主窗口界面
-//
+*/
 #include "chartwidget.h"
 #include "ui_chartwidget.h"
 #include <QDebug>
